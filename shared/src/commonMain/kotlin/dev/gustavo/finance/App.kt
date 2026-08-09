68224d39ba7e1a546102f7cb257e493c8a206af5
Gustavo Kieling <gustavoknz@gmail.com>
2026-08-09 11:52:13 +0100

    Remove unused code

--- shared/src/commonMain/kotlin/dev/gustavo/finance/App.kt
+++ shared/src/commonMain/kotlin/dev/gustavo/finance/App.kt
@@ -3,7 +3,10 @@ package dev.gustavo.finance
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import dev.gustavo.finance.di.commonModule
+import dev.gustavo.finance.di.databaseModule
+import dev.gustavo.finance.di.domainModule
+import dev.gustavo.finance.di.networkModule
+import dev.gustavo.finance.di.viewModelModule
 import dev.gustavo.finance.presentation.rates.ExchangeRateScreen
 import org.koin.compose.KoinApplication
 import org.koin.dsl.koinConfiguration
@@ -19,7 +22,12 @@ fun App() {
 @Preview
 fun AppPreview() {
     KoinApplication(configuration = koinConfiguration {
-        modules(commonModule)
+        modules(
+            networkModule,
+            databaseModule,
+            domainModule,
+            viewModelModule
+        )
     }) {
         App()
     }
--- shared/src/commonMain/kotlin/dev/gustavo/finance/GreetingUtil.kt
+++
@@ -1,3 +0,0 @@
-package dev.gustavo.finance
-
-fun sayHello(to: String): String = "Hello, $to!"
